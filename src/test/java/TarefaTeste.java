6c9c6d9ca6408663b745aa4cc9cb5d14c3093e4b
Savanna Denega Machado SICREDI <savanna_machado@terceiros.sicredi.com.br>
2018-09-04 00:10:59 -0300

    Adicionando classe do Mockito

--- src/test/java/TarefaTeste.java
+++ src/test/java/TarefaTeste.java
@@ -1,10 +1,37 @@
 
+import com.abcontrol.Dao.TarefaDao;
+import com.abcontrol.Entity.Tarefa;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
 
-public TarefaTeste{
+import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
+public class TarefaTeste{
 
+    @Mock
+    TarefaDao tarefaDao;
+
+    @Before
+    public void init(){
+        MockitoAnnotations.initMocks(this);
+        tarefaDao = new TarefaDao();
+    }
+
+    @Test
+    public void testargetTarefaById(){
+        when(tarefaDao.getTarefaById(0)).thenReturn(new Tarefa());
+        Tarefa tarefa = tarefaDao.getTarefaById(0);
+        Assert.assertEquals(0, tarefa.getId());
+        Assert.assertEquals("Gerar relatório projetos", tarefa.getNome());
+        Assert.assertEquals("Gerar relatório de todos os projetos em execução hoje", tarefa.getDescricao());
+        Mockito.verify(tarefaDao, Mockito.atLeastOnce()).getTarefaById(0);
+    }
 
 }
